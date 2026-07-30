16b95b7dd592b9f2d03f197d3f6eb2a61eae3358
Joshua Buck <me.joshuabuck@gmail.com>
2026-07-30 10:42:49 -0400

    moved provider varibales to the provider file since they will only use used there once. removed references to username and password since using token authentication.

--- provider.tf
+++ provider.tf
@@ -4,10 +4,27 @@ terraform {
     required_providers {
         proxmox = {
             source  = "telmate/proxmox"
-            version = ">=3.0.2"
+            version = "3.0.2-rc08"
         }
     }
 }
+
+variable "pve_host" {
+  description = "Proxmox host for API access"
+  type        = string
+}
+
+variable "pve_token_id" {
+  description = "Proxmox API token ID for API access"
+  type        = string
+}
+
+variable "pve_token_secret" {
+  description = "Proxmox API token secret for API access"
+  type        = string
+  sensitive   = true
+}
+
 provider "proxmox" {
     pm_api_url      = "https://${var.pve_host}:8006/api2/json"
     pm_api_token_id = var.pve_token_id
